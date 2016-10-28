759ab2c06d73dfb99da1ac8f6af210729ab0e672
David Zambrano <davazamb10@outlook.com>
2016-10-28 12:35:51 -0400

    Meotod main, suma

---
+++ ClassCSharp/HolaMundo.cs
@@ -0,0 +1,80 @@
+﻿//los "using" es poner las clases que reciben lo que hay dentro del namespace. 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Se declara el namespace: esto significa que todas las clases que se creen aca solo seran accedidas atraves de namespace
+namespace ClassCSharp
+{
+    //La llave define lo que estara dentro y se le llama bloque a lo que esta en el interior de las llaves.
+    class HolaMundo
+    {
+        //Ejemplo de hola mundo
+        //Creamos clase "Console": Permite mostrar mensaje en la consola
+        //le daremos punto a Console. Aca mostrara los miembros de la clase "Console" 
+        //entre parentesis y comillas es lo que le pasamos en el mensaje de console.
+        //Esta es una sentencia la cual debe terminar en punto y coma ;
+        //Sentencia es una instrucion que le damos a la computadora. en C# estas terminan en ;
+        //Ejemplo:
+
+            //El metodo main sera el punto de inicio de la aplicacion
+            //Los metodos son bloques de codigo que podemos ejecutar cuantas veces queramos.
+            //pueden recibir valores de entrada, llamado parametros. tambien puede devolver valores si se desea.
+        static void Main1(string[] args)
+        {
+            //Metodos miembros que realizan una accion
+            //WriteLine es el metodo que escribe en pantalla
+            Console.WriteLine("Hola Mundo");
+            //Read es el metodo que lee y muestra mensaje.
+            Console.Read();
+        }
+    }
+    //>>>>VARIABLES<<<<<
+
+    //Una variable debera contener una sintaxis;
+    //tipo de identificador: 
+    //tipo:  dato que se almacena la variable, puede ser numero, entero, cadena de caracteres, fecha , booleano y otros...
+    // Identificador: es el nombre de la variable.
+    //public class Variables
+    //{
+    //static void Main(string[] args)
+    //{
+    //Ejemplo de declaracion de una variable:
+    //Numeros enteros
+
+             //int mes;
+
+    //Cadenas de textos
+
+             //string nombre;
+
+    //Tipo de datos de fecha
+
+             //DateTime fechaEntrada;
+
+    //Son valores verdaderos y falsos
+
+            //bool procesoTerminado;
+
+    //Ejemplo iniciando variable;
+
+            //mes = 7;
+
+    //Se escribe entre doble comilla porque es cadena de texto.
+
+
+            //nombre = "David";
+            //procesoTerminado = true;
+            //procesoTerminado = false;
+
+
+    //Se puede inicializar y declarar en una misma linea
+
+
+           //string direccion = "avenida 2323";
+
+    //}
+    //}
+}
---
+++ ClassCSharp/HolaMundo2.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassCSharp
+{
+    class HolaMundo2
+    {
+        static void Main2(string[] args)
+        {
+            EscribeHolaMundo();
+            Console.WriteLine("Me llamo David");
+            IdentificadorDelMetodo("valor1", "valor1", 6);
+            int sumando1 = 23;
+            int sumando2 = 72;
+            int resultado = Suma(sumando1, sumando2);
+            Console.WriteLine(resultado);
+            Console.Read();
+
+        }
+        //Metodo que devolvera un valro
+        static int Suma(int a, int b)
+        {
+            //para devolver un valor se usa return
+            return a + b; 
+        }
+
+
+
+        //Creamos un metodo escribeholamundo
+        //void EscribeHolaMundo() El cual debe ser estatico
+        //Dentro de un metodo estatico de una clase solo puedes llamar otros metodos estaticos de la misma clase,
+        //o a menos que instancies la clase.
+        static void EscribeHolaMundo()
+        {
+            Console.WriteLine("Hola Mundo");
+        }
+
+        //Cadena de texto para escribir en consola pero debe incluir los paramatros (string valor)
+        static void EscribeEnLaConsola(string valor)
+        {
+            Console.WriteLine(valor);
+        }
+
+        //sisntasis de un metodo estatico se pueden colocar tantas variables de entrada
+        //los valores se declaran en el main
+        //Void> son aquellos metodos que no tiene valores de salida, es decir que no devuelve un resultado de quien los invoca.
+        static void IdentificadorDelMetodo(string parametro1, string parametro2, int etc)
+        {
+            
+        }
+    }
+}
